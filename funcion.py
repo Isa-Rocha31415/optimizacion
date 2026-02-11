c281a932ab04c026c5f1a6714b52fb335c2f0a2c
Rapla913 <92961696+Rapla913@users.noreply.github.com>
2026-02-11 16:56:08 -0600

    Added function.py

---
+++ funcion.py
@@ -0,0 +1,17 @@
+
+class function: 
+
+    def __init__(self, function): 
+        pass
+
+    def eval(): 
+        pass
+    
+    def diff():
+        pass 
+
+    def ddiff(): 
+        pass
+
+    def plot(): 
+        pass
