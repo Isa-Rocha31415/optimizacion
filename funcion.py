e6e0b6b9bc3d6ea3a127be7dde26ac26f9d2a88e
Rapla913 <92961696+Rapla913@users.noreply.github.com>
2026-02-11 17:21:27 -0600

    Added prototypes

---
+++ funcion.py
@@ -0,0 +1,21 @@
+import numpy 
+
+class funcion: 
+    # Variables 
+    func = None 
+    # end variables 
+
+
+    # Methods 
+    def __init__(self, fun):
+        pass
+
+    def eval(): 
+        pass 
+
+    def diff(): 
+        pass 
+
+    def doif(): 
+        pass 
+    # end methods
