588b2f4ef97edcc36d9afdd5c19d19fac4e4825a
Isabel <isabelrocha31415@gmail.com>
2026-02-16 23:34:53 -0600

    cambios isa class_dg y main

--- Tarea_1/main.py
+++ Tarea_1/main.py
@@ -9,5 +9,20 @@ from class_dg import dg
 if __name__ == "__main__":
 
     #unimos todo:
+    fig, plot = plt.subplots(figsize=(10,8))
+
+    #func = sphere
+    #func = cigar
+    func = rosenbrock
+
+    desenso_gradiente =dg(func,paso=0.0004)
+    punto_inicial = [0,0]
+    desenso_gradiente.solve(x_o =punto_inicial)
+
+    func.plot(lim=[-5,5], canvas =ax)
+    desenso_gradiente.plot2d(canvas=ax)
+
+    plt.title(f"Optimizacion de {type(func).__name__}")
+    plt.show()
 
     pass
