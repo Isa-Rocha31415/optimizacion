a7210df723cede1adb710e93a3909595b7282cb8
Rapla913 <92961696+Rapla913@users.noreply.github.com>
2026-02-21 22:46:59 -0600

    Añadida función banana n-dimensional

--- Tarea_2/class_rosenbrock.py
+++ Tarea_2/class_rosenbrock.py
@@ -2,21 +2,25 @@ from funcion import funcion
 import numpy as np
 
 class rosenbrock(funcion):
-    # RAPLA  f(x, y) = (1 - x)**2 + 100*(y - x**2)**2
+    # RAPLA  f(x_1, x_2, x_3, ..., x_n ) = Σ(1 - x_i)**2 + 100*(x_i+1 - x_i**2)**2
 
     def eval(self, args: np.array) -> int:
         """
         Método que evalúa la función en un punto particular
         
-        :param args: Punto en el que se evalúa la función tal que (x, y)
+        :param args: Punto ndimensional para evaluar
         :type args: np.array
         :return: Valor de la función en el punto dado 
         :rtype: int
         """
-        x = args[0] 
-        y = args[1]
+        result = 0
 
-        result = (1 - x)**2 + 100*(y - x**2)**2
+        for i in range(len(args) - 1): 
+            x = args[i]
+            y = args[i+1] 
+            
+            result += (1 - x)**2 + 100*(y - x**2)**2
+            
         return result 
         
 
