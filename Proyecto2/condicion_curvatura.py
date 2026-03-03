035ed92be1f47a33ff400251b25f6cd3c4195db8
crisad_10 <torrescristopher839@gmail.com>
2026-03-03 16:44:11 -0600

    completada curvatura

--- Proyecto2/condicion_curvatura.py
+++ Proyecto2/condicion_curvatura.py
@@ -1,3 +1,8 @@
-def c_scobertura():
-    #recuerda deben ser la misma cantidad de argumentos
-    pass
+import numpy as np
+def curvatura(f_k, f_k1, g_k, P_k, alpha_k):
+
+    c2 = 0.9
+    pendiente_actual = np.dot(g_k, P_k)
+    pendiente_nueva_aprox = (f_k1 - f_k) / alpha_k
+    return pendiente_nueva_aprox < c2 * pendiente_actual
+    pass    
