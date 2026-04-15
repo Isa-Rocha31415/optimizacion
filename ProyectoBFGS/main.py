b3f40e7d588d76d44937b8caa7938774f1663d94
Isabel <isabelrocha31415@gmail.com>
2026-04-15 16:41:23 -0600

    main modificado

--- ProyectoBFGS/main.py
+++ ProyectoBFGS/main.py
@@ -1,42 +1,13 @@
-from class_GC import GC
-from class_funcion import funcion as softened 
+from class_BFGS import BFGS
 from PIL import Image 
 import numpy as np
 import matplotlib.pyplot as plt
 
-img_path = r"./ProyectoGradienteConjugado/img/fractal_tree.ascii.pgm" 
+img_original = r"./ProyectoBFGS/img/I_1.pgm" 
+img_chueca = r"./ProyectoBFGS/img/I_6.pgm" 
 
 if __name__ == "__main__" : 
     # Leer la imagen con Pillow 
-    img = np.array(Image.open(img_path)).astype(float)
-    img_norm = img /255.0
-    img_shape =img_norm.shape
+    img_original = np.array(Image.open(img_original)).astype(float)
+    img_chueca = np.array(Image.open(img_chueca)).astype(float)
     
-    lambdas_to_test = [32.0]
-    fig, axes = plt.subplots(1, len(lambdas_to_test) + 1, figsize=(20, 5))
-
-    axes[0].imshow(img_norm, cmap='gray')
-    axes[0].set_title("Original")
-    axes[0].axis('off')
-
-    for i, lmd in enumerate(lambdas_to_test):
-        print(f"Procesando Lambda = {lmd}...")
-        
-        # Instanciar con el lambda actual
-        obj_func = softened(img_norm, lamda=lmd)
-        algorithm = GC(obj_func)
-        
-        # Resolvemos (Pasamos la imagen plana como punto inicial)
-        result_flat = algorithm.solve(img_norm.flatten())
-        
-        # Reconstruir y mostrar
-        img_out = np.reshape(result_flat, img_shape)
-        
-        axes[i+1].imshow(img_out, cmap='gray', vmin=0, vmax=1)
-        axes[i+1].set_title(f"λ = {lmd}")
-        axes[i+1].axis('off')
-
-    plt.tight_layout()
-    plt.savefig("./ProyectoGradienteConjugado/img/evidencia_lambdas.png", dpi=300)
-    print("\n¡Listo! Comparativa guardada en 'img/evidencia_lambdas.png'")
-    plt.show()
